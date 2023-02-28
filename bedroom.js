af49ad9acf1cdb9a44dafb0a9dae20f5cf1685dd
neilsydney43 <101984283+neilsydney43@users.noreply.github.com>
2023-02-27 19:46:28 -0500

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,66 @@
+img="";
+Status="";
+objects=[];
+function preload()
+{
+  img=loadImage("b3-image.png");
+}
+
+function setup()
+{
+  canvas=createCanvas(640,420);
+  canvas.center();
+
+  objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+
+}
+
+function modelLoaded()
+{
+  console.log("Model Loaded!");
+  Status=true;
+
+}
+
+function gotResult(error,result)
+{
+  if(error)
+  {
+    console.log(error);
+  }
+  console.log(result)
+  objects=result;
+}
+
+function draw()
+{
+
+  image(img,0,0,640,420)
+  if(Status !="")
+  {
+    r=random(255)
+    g=random(255)
+    b=random(255)
+
+
+
+    objectDetector.detect(img,gotResult);
+    for(var i=0;i<objects.length;i++)
+    {
+
+
+  fill(r,g,b);
+  text(objects[i].label,objects[i].x+15,objects[i].y-15);
+  noFill();
+  stroke(r,g,b);
+  rect(objects[i].x+15,objects[i].y-15,objects[i].width,objects[i].height);
+    }
+  }
+
+ 
+}
+
+function back()
+{
+    window.location="index.html";
+}
---
+++ main.js
@@ -0,0 +1,20 @@
+function button_1()
+{
+    window.location="garden.html";
+}
+function button_2()
+{
+    window.location="kitchen.html";
+}
+function button_3()
+{
+    window.location="bedroom.html";
+}
+function button_4()
+{
+    window.location="study.html";
+}
+function button_5()
+{
+    window.location="mall.html";
+}
